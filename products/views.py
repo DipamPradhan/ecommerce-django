a7a6333d8dade1ea6dd34f9172036f80beb45459
Dipam Pradhan <meedipam@gmail.com>
2026-02-09 21:30:42 +0545

    permissions and authorizations

--- products/views.py
+++ products/views.py
@@ -2,24 +2,25 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Product
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 
 
-class ProductListView(ListView):
+class ProductListView(LoginRequiredMixin, ListView):
     model = Product
     template_name = "products/product_list.html"
     context_object_name = "products"
     paginate_by = 10
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
     template_name = "products/product_detail.html"
     context_object_name = "product"
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     template_name = "products/product_add.html"
     fields = [
@@ -28,17 +29,28 @@ class ProductCreateView(CreateView):
         "price",
         "description",
         "stock",
-        "seller",
     ]
 
+    def form_valid(self, form):
+        form.instance.seller = self.request.user
+        return super().form_valid(form)
 
-class ProductUpdateView(UpdateView):
+
+class ProductUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Product
     template_name = "products/product_update.html"
     fields = ["name", "image", "price", "description", "stock"]
 
+    def test_func(self):
+        product = self.get_object()
+        return self.request.user == product.seller
 
-class ProductDeleteView(DeleteView):
+
+class ProductDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Product
     template_name = "products/product_delete.html"
     success_url = reverse_lazy("product_list")
+
+    def test_func(self):
+        product = self.get_object()
+        return self.request.user == product.seller
